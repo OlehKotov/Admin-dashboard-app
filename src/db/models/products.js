ced7a0e1e113354a9d39d2081c515526331f82c9
OlehKotov <mail.oleg456@gmail.com>
2024-11-21 16:54:23 +0200

    Update products.js

--- src/db/models/products.js
+++ src/db/models/products.js
@@ -2,14 +2,6 @@ import { model, Schema } from 'mongoose';
 
  const productsSchema = new Schema(
    {
-    // id: {
-    //    type: String,
-    //    required: true,
-    //  },
-     photo: {
-        type: String,
-        required: true,
-      },
       name: {
         type: String,
         required: true,
