11337d03175ea8176f01bd5802465f317f44820b
OlehKotov <mail.oleg456@gmail.com>
2024-12-05 17:49:58 +0200

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -4,6 +4,7 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
+  phone: Joi.string().pattern(/^\+380\d{9}$/).required(),
   password: Joi.string().required(),
 });
 
